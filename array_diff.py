6564866c244e277aeacb838eb32625b088fc33ce
Maxim Sidorenko <msidorenko@phoenixit.ru>
2020-11-24 00:31:15 +0300

    solve sum_two_low_pos_int problem

--- array_diff.py
+++ array_diff.py
@@ -21,7 +21,7 @@ def array_diff(a: list, b: list) -> list:
 print(array_diff([1, 2, 2, 2, 3, 4], [2, 4]))
 
 """
-Best Practices:
+Other solutions
 1.
 def array_diff(a, b):
     return [x for x in a if x not in b]
