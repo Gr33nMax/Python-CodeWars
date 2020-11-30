87f0bcb41b61f275e6e8697b1c95c987dfea5490
Maxim Sidorenko <msidorenko@phoenixit.ru>
2020-11-30 14:03:00 +0300

    solve max_subarr_sum problem

--- max_subarr_sum.py
+++ max_subarr_sum.py
@@ -20,10 +20,10 @@ def max_sequence(arr: list) -> int:
     def gen_sequences():
         for i in range(len(arr)):
             for j in range(i + 1, len(arr) + 1):
-                yield reduce(lambda a, b: a + b, arr[i:j]), arr[i:j]
+                yield reduce(lambda a, b: a + b, arr[i:j])
 
     result = gen_sequences()
-    return max(result)[0]
+    return max(result)
 
 
 print(max_sequence([7, 4, 11, -11, 39, 36, 10, -6, 37, -10, -32, 44, -26, -34, 43, 43]))
